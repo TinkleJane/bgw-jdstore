0339ae168af76d001248128825b85fd71d729392
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-04 09:59:18 +0800

    base modify

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'welcome#index'
+
+  resources :welcome do
+    collection do
+      get :about
+    end
+  end
 
   namespace :admin do
     resources :products
