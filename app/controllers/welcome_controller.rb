dcb90d901f9dcc61791f8ac21de6b42338b56da3
zhaoxuejiao <tinklexjane@gmail.com>
2017-06-10 19:34:07 +0800

    add active pic to show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
 
   def index
     @products = Product.onsaled.where(:is_command => true).take(2)
+    @products_show = Product.onsaled.order("position ASC").where(:is_command => false).take(5)
   end
 end
